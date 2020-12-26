20311f34e90ddfe9034f8c8a32ea266321f0d45c
Yaroslav Kukytsyak <rosktsk@gmail.com>
2020-12-26 16:10:42 +0100

    Reduce bundle size by optimizing autoRetryOnRejection
    Reduced the bundle size from almost 3 KB to less than 600 B (5x
    reduction) by making autoRetryOnRejection a regular function rather than
    async function. Using an async function causes TypeScript to emit
    helper code, increasing the bundle size by about 2.4 KB.
    Now, we instead just use promise methods and recursion to achieve the
    same behavior we had with an async function.

--- src/utils/promise/autoRetryOnRejection/index.ts
+++ src/utils/promise/autoRetryOnRejection/index.ts
@@ -4,6 +4,14 @@ interface Options {
   timeoutMs?: number;
 }
 
+const autoRetryOnRejectionRec = <T extends any>(
+  fn: () => Promise<T>,
+  options: Required<Options>,
+): Promise<T> =>
+  fn().catch(() =>
+    wait(options.timeoutMs).then(() => autoRetryOnRejectionRec(fn, options)),
+  );
+
 /**
  * Given a function that returns a promise, keeps calling it until the returned
  * promise resolves, returning the result. If the promise rejects, waits
@@ -11,19 +19,11 @@ interface Options {
  * @param fn The function that returns a promise.
  * @param options Options that can be used to adjust the retry timeout (default 3000).
  */
-export const autoRetryOnRejection = async <T extends any>(
+export const autoRetryOnRejection = <T extends any>(
   fn: () => Promise<T>,
   options: Options = {},
 ) => {
   const { timeoutMs = 3000 } = options;
 
-  while (true) {
-    try {
-      const result = await fn();
-
-      return result;
-    } catch {
-      await wait(timeoutMs);
-    }
-  }
+  return autoRetryOnRejectionRec(fn, { timeoutMs });
 };
